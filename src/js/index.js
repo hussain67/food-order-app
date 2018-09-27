157e4abaed75fb510be8924b353e36f7700ae4ea
Shahid <hussain.msh67@yahoo.com>
2018-09-27 22:06:20 +0300

    Api call with axios successfully completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
-import string from './models/Search'
+//API key c18ec3820752eb04e570509bbb5723cb
+// https://www.food2fork.com/api/search 
+// https://www.food2fork.com/api/get
 
-//import {add as a, multiply, ID} from './views/searchViews'
-import * as searchViews from './views/searchViews'
-console.log(`Imported functions check ${searchViews.ID, 2}; ${searchViews.add(2, 4)} and ${searchViews.multiply(3, 5)}. ${string}`);
+import axios from 'axios';
+
+async function getResults(query){
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const key = 'c18ec3820752eb04e570509bbb5723cb';
+    try{
+      const res = await axios(`${proxy}www.food2fork.com/api/search?key=${key}&q=${query}`);
+      const recipes = res.data.recipes;
+      console.log(recipes)
+    }catch(error){
+      alert(error);
+    }
+  ;
+
+}
+getResults('drinks') ;
